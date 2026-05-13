6021168f80088819b4a972fdb0263d538cf29b6b
Jonas "El  pipa" LECHERSON <49516747@est.ort.edu.ar>
2026-05-13 14:28:46 -0300

    nicho

--- src/Backend/registro.js
+++ src/Backend/registro.js
@@ -1,6 +1,6 @@
 export const prerender = false;
 import sql from '../carga.js';
-import bcrypt from 'bcryptjs';
+import { argon2id } from 'hash-wasm';
 
 export async function POST({ request }) {
   try {
@@ -9,22 +9,33 @@ export async function POST({ request }) {
     if (!usuario || !contraseña) {
       return new Response(JSON.stringify({ error: "Datos incompletos" }), { status: 400 });
     }
+
     const existe = await sql`SELECT "ID" FROM "Usuarios" WHERE "Nombre" = ${usuario}`;
     
     if (existe.length > 0) {
       return new Response(JSON.stringify({ error: "El usuario ya existe" }), { status: 400 });
     }
 
-    const hash = await bcrypt.hash(contraseña, 10);
-
+    const salt = crypto.getRandomValues(new Uint8Array(16)); 
+    const hash = await argon2id({
+      password: contraseña,
+      salt: salt,
+      parallelism: 1,
+      iterations: 2,
+      memorySize: 16384, // 16MB
+      hashLength: 32,
+      outputType: 'encoded', // Esto genera el string largo que guardaremos
+    });
+    
     await sql`
       INSERT INTO "Usuarios" ("Nombre", "Contraseña") 
       VALUES (${usuario}, ${hash})
     `;
 
-    return new Response(JSON.stringify({ success: true }), { status: 201 });
+    return new Response(JSON.stringify({ success: true, message: "Usuario creado" }), { status: 201 });
+
   } catch (error) {
     console.error("Error en registro:", error.message);
-    return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+    return new Response(JSON.stringify({ error: "Error interno del servidor" }), { status: 500 });
   }
 }
